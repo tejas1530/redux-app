eedb9ebe6e52f0c5532412838cfb3a514ecdabcf
TejasPickupBiz <tejaspickupbiz@gmail.com>
2023-11-06 23:52:25 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const App = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <h1>hello</h1>
+    </React.Fragment>
+  );
 };
 export default App;
